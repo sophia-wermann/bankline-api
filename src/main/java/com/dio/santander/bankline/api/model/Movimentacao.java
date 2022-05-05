5ea6c5ec43c541cb66f6c004a38a77d42d1f689b
SophiaWermann <00302355@ufrgs.br>
2022-05-05 03:20:34 -0300

    versão final do nosso projeto

--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -27,6 +27,10 @@ public class Movimentacao {
 	
 	@Enumerated(EnumType.STRING)
 	private MovimentacaoTipo tipo;
+	
+	@Column(name = "id_conta")
+	private Integer idConta;
+	
 	public Integer getId() {
 		return id;
 	}
@@ -56,6 +60,14 @@ public class Movimentacao {
 	}
 	public void setTipo(MovimentacaoTipo tipo) {
 		this.tipo = tipo;
+	}
+	
+	public Integer getIdConta() {
+		return idConta;
+	}
+	
+	public void setIdConta(Integer idConta) {
+		this.idConta = idConta;
 	}
 	
 }
